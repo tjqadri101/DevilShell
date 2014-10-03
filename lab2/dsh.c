5c00e1fe00ea3133fdeddce2f75877fe31030364
Talal Javed Qadri <tq4@teer12.oit.duke.edu>
2014-10-02 21:14:05 -0400

    Added fg command support

--- lab2/dsh.c
+++ lab2/dsh.c
@@ -7,6 +7,7 @@ void set_job_status(job_t *j);/*set process status for a spawned job*/
 
 job_t *active_list = NULL; //active list of jobs
 
+
 /* Sets the process group id for a given job and process */
 int set_child_pgid(job_t *j, process_t *p)
 {
@@ -119,26 +120,32 @@ void spawn_job(job_t *j, bool fg)
 	seize_tty(getpid()); // assign the terminal back to dsh
 	
 }
-
+//Sets the status int of a process_t, as well as the completed and stopped bools
+//Method ensures that these values are updated after a process has completed
 void set_job_status(job_t *j){
 	process_t *p;
+	pid_t pid;
 	for(p = j->first_process; p; p = p->next){
-		while(waitpid(p->pid, &p->status, WNOHANG|WUNTRACED) == 0); //waitpid returns 0 here if child has not terminated; otherwise returns child pid
+		
+		while((pid = waitpid(p->pid, &p->status, WNOHANG|WUNTRACED)) == 0); //waitpid returns 0 here if child has not terminated; otherwise returns child pid or negative number on error
 			//p has exited by now, either normally or abnormally		
-			if (WIFEXITED(p->status)){
-				printf("Process %d of job %ld terminated normally with exit status=%d\n",p->pid, (long)j->pgid, WEXITSTATUS(p->status));
-				p->completed = true;
+			if(pid < 0){
+				perror("Process exited abnormally");
+				exit(EXIT_FAILURE);
 			}
-			else if (WIFSTOPPED(p->status)){
-				printf("Process %d of job %ld stopped by signal number=%d\n",p->pid, (long)j->pgid, WSTOPSIG(p->status));
-				p->stopped = true;
-			}
-			else if (WIFSIGNALED(p->status)){
-				printf("Process %d of job %ld terminated by signal number=%d\n",p->pid, (long)j->pgid, WTERMSIG(p->status));
-				p->completed = true;
-			}
-			else if (WIFSIGNALED(p->status)){
-				printf("Process %d of job %ld terminated by signal number=%d\n",p->pid, (long)j->pgid, WTERMSIG(p->status));
+			else{
+				if (WIFEXITED(p->status)){
+					printf("Process %d of job %ld terminated normally with exit status=%d\n",p->pid, (long)j->pgid, WEXITSTATUS(p->status));
+					p->completed = true;
+				}
+				else if (WIFSTOPPED(p->status)){
+					printf("Process %d of job %ld stopped by signal number=%d\n",p->pid, (long)j->pgid, WSTOPSIG(p->status));
+					p->stopped = true;
+				}
+				else if (WIFSIGNALED(p->status)){
+					printf("Process %d of job %ld terminated by signal number=%d\n",p->pid, (long)j->pgid, WTERMSIG(p->status));
+					p->completed = true;
+				}
 			}
 	}
 }
@@ -151,6 +158,42 @@ void continue_job(job_t *j)
           perror("kill(SIGCONT)");
 }
 
+/*Talal's code block here*/
+//Given a pgid and a head of the job list, returns the job_t with the pgid in the list, if no such job then returns null
+job_t* find_job_in_list(pid_t pgid, job_t *first_job){
+	if(!pgid || !first_job)
+		return NULL;
+	job_t *cur_job;
+	cur_job = first_job;
+	while(cur_job != NULL) {
+		if(cur_job->pgid == pgid)
+			return cur_job;
+		cur_job = cur_job->next;
+	}
+	//job not found in list
+	printf("Job with group id = %d was not found.\n", pgid);
+	return NULL;
+
+}
+/*Wakes up a blocked job, updates the status of the blocked processes to false, then updates a process's overall status flags after a process has completed*/
+void wake_update_job(job_t *j){
+	process_t *p;
+	continue_job(j);
+	printf("Resuming job with group id = %d\n",j->pgid);
+	//update stopped field of the processes
+	for(p = j->first_process; p; p = p->next){
+		p->stopped = false;
+	}
+	//for each process of the job, see the status at the end of process (completed, stopped, interrupted) and set the flags in the process_t sturct appropriately*/
+	set_job_status(j);
+	if(job_is_stopped(j)){
+		if(job_is_completed(j)){
+			delete_job(j, active_list); //remove from active list
+		}
+		//else if job suspended again, leave in active_list
+	}
+}
+/*Talal's code ends*/
 
 /* 
  * builtin_cmd - If the user has typed a built-in command then execute
@@ -168,11 +211,13 @@ bool builtin_cmd(job_t *last_job, int argc, char **argv)
             exit(EXIT_SUCCESS);
 	}
         else if (!strcmp("jobs", argv[0])) {
-            /* Your code here */
+            // Only prints out the suspended jobs that have not yet completed 
+	    //The user is notified of completed jobs when the prompt appears again...
+            //which means that the job has completed and the shell is running in the foreground again
+	    print_job(active_list->next);
             return true;
         }
 	else if (!strcmp("cd", argv[0])) {
-            /* Your code here */
 		if(chdir(argv[1]) < 0)
 			perror("Incorrect directory path was given.");
 		return true;
@@ -181,17 +226,33 @@ bool builtin_cmd(job_t *last_job, int argc, char **argv)
             /* Your code here */
         }
         else if (!strcmp("fg", argv[0])) {
-            /* Your code here */
+            if(argv[1]){//the pgid was given as an argument
+		int pgid = (int) (strtol(argv[1], NULL, 0));
+		wake_update_job(find_job_in_list(pgid, active_list));
+	    }
+	    else{//No pgid was given as an argument
+		wake_update_job(find_last_job(active_list));
+	    }
+	    return true;
         }
         return false;       /* not a builtin command */
 	/*Talals code ends here*/
 }
+/*Talal's code block here*/
+void append_to_joblist(job_t *j, job_t * head){
+	j->next = NULL;
+	find_last_job(head)->next = j;
+}
+
+
+
+/*Talal's code block ends here*/
 
 /* Build prompt messaage */
 char* promptmsg() 
 {
     /* Modified by Talal to include pid */
-	char *prompt = ((char *) (malloc(sizeof(char)*30)));//4 bytes needed for pid int, 1 for null character in the end
+	char *prompt = ((char *) (malloc(sizeof(char)*50)));//4 bytes needed for pid int, 1 for null character in the end
 	int pid = getpid();
 	sprintf(prompt, "dsh(PID = %d)$ ", pid);
 	return prompt;
@@ -215,66 +276,69 @@ int main()
         }
 	/*End of Talal's code block*/
 	while(1) {
-	/*The block starting here was modified by Talal*/
-	char* prompt;
-	prompt = promptmsg();
-		if(!(j->next = readcmdline(prompt))) {
-			if (feof(stdin)) { /* End of file (ctrl-d) */
-				fflush(stdout);
-				printf("\n");
-				exit(EXIT_SUCCESS);
-           		}
-			continue; /* NOOP; user entered return or spaces with return */
-		}
-	free(prompt);//free the pointer to the prompt message
-	/*End of modified block*/
-        /* Only for debugging purposes to show parser output; turn off in the
-         * final code */
-        if(PRINT_INFO) print_job(j->next);
-
-        /* Your code goes here */
-        /* You need to loop through jobs list since a command line can contain ;*/
-        /* Check for built-in commands */
-        /* If not built-in */
-            /* If job j runs in foreground */
-            /* spawn_job(j,true) */
-            /* else */
-            /* spawn_job(j,false) */
-
-
-	/* This block was added by Talal and Negatu */
-	job_t *temp_job; //current job
-	job_t *cur_job = j->next;
-	while(cur_job != NULL) {
-	//As per instructions, if a job contains a builtin command then it is the only command in the job
-		if(!builtin_cmd(cur_job, cur_job->first_process->argc, cur_job->first_process->argv)){
-			if (cur_job->bg){
-				spawn_job(cur_job,false);
+		/*The block starting here was modified by Talal*/
+		char* prompt;
+		prompt = promptmsg();
+			if(!(j->next = readcmdline(prompt))) {
+				if (feof(stdin)) { /* End of file (ctrl-d) */
+					fflush(stdout);
+					printf("\n");
+					exit(EXIT_SUCCESS);
+					}
+				continue; /* NOOP; user entered return or spaces with return */
 			}
-			else{
-				spawn_job(cur_job,true);
-			}
-			if(job_is_stopped(cur_job)){
-				if(job_is_completed(cur_job)){
-					temp_job = cur_job->next;
-					delete_job(cur_job, j);
-					cur_job = temp_job;
+		free(prompt);//free the pointer to the prompt message
+		/*End of modified block*/
+			/* Only for debugging purposes to show parser output; turn off in the
+			 * final code */
+			//if(PRINT_INFO) print_job(j->next);
+
+			/* Your code goes here */
+			/* You need to loop through jobs list since a command line can contain ;*/
+			/* Check for built-in commands */
+			/* If not built-in */
+				/* If job j runs in foreground */
+				/* spawn_job(j,true) */
+				/* else */
+				/* spawn_job(j,false) */
+
+
+		/* This block was added by Talal and Negatu */
+		job_t *temp_job; //current job
+		job_t *cur_job = j->next;
+		while(cur_job != NULL) {
+		//As per instructions, if a job contains a builtin command then it is the only command in the job
+			if(!builtin_cmd(cur_job, cur_job->first_process->argc, cur_job->first_process->argv)){
+				if (cur_job->bg){
+					spawn_job(cur_job,false);
 				}
 				else{
-					cur_job = cur_job->next;
+					spawn_job(cur_job,true);
 				}
+				if(job_is_stopped(cur_job)){
+					if(job_is_completed(cur_job)){
+						temp_job = cur_job->next;
+						delete_job(cur_job, j);
+						cur_job = temp_job;
+					}
+					else{//job was stopped so add to active list
+						temp_job = cur_job;
+						cur_job = cur_job->next;
+						append_to_joblist(temp_job, active_list);
+					}
 
 
+				}
+				
 			}
-		    else{//command was a built in command
-			cur_job = cur_job->next;
+			else{//command was a built in command
+				cur_job = cur_job->next;
 
-		   } 
+			} 
+
 		}
-
-	}
-	print_job(j->next);
-	/* Block ends here*/
+		
+		/* Block ends here*/
 
 
 
